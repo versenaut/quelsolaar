472b00444250702df0e74cb51791444ea4e9ac93
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-04-22 08:38:49 +0000

    Reflowed
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2276 49c17534-1d02-dc11-b283-0030488c597c

--- co_widgets.h
+++ co_widgets.h
@@ -2,15 +2,9 @@
  * 
 */
 
-extern boolean	co_w_checkbox(BInputState *input, float pos_x, float pos_y, boolean *state,
-			      float red, float green, float blue);
-extern boolean	co_w_type_in(BInputState *input, float pos_x, float pos_y, float length, float size,
-			     char *text, uint buffer_size,
-			     void (*done_func)(void *user, char *text), void* user, float text_color, float line_color);
-extern boolean	co_w_slider(BInputState *input, float pos_x, float pos_y, float length, double *nr,
-			    float red, float green, float blue);
+extern boolean co_w_checkbox(BInputState *input, float pos_x, float pos_y, boolean *state, float red, float green, float blue);
+extern boolean co_w_type_in(BInputState *input, float pos_x, float pos_y, float length, float size, char *text, uint buffer_size, void (*done_func)(void *user, char *text), void* user, float text_color, float line_color);
+extern boolean co_w_slider(BInputState *input, float pos_x, float pos_y, float length, double *nr, float red, float green, float blue);
 
-extern void	sui_draw_rounded_square(float pos_x, float pos_y, float size_x, float size_y,
-					float red, float green, float blue);
-extern boolean	co_w_close_button(BInputState *input, float pos_x, float pos_y,
-				  float red, float green, float blue);
+extern void sui_draw_rounded_square(float pos_x, float pos_y, float size_x, float size_y, float red, float green, float blue);
+extern boolean co_w_close_button(BInputState *input, float pos_x, float pos_y, float red, float green, float blue);
