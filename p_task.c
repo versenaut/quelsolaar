8c2174263bec18f26bb09a915737e47ff835e2c8
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-06-13 11:34:08 +0000

    Protect against multi-adds
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2607 49c17534-1d02-dc11-b283-0030488c597c

--- p_task.c
+++ p_task.c
@@ -5,12 +5,13 @@
 	#include <windows.h>
 	#include <GL/gl.h>
 #else
-	#include <gl.h>
+	#include <GL/gl.h>
 #endif
 #include "verse.h"
 #include "enough.h"
 
 
+#include "p_extension.h"
 #include "p_shader.h"
 #include "p_task.h"
 
@@ -33,13 +34,21 @@ void p_task_add(uint id, float importance, boolean (*func)(uint id))
 	PTask *t;
 	{
 		ENode *n; 
-		uint *a = NULL;
+		uint *a = NULL, i;
 		n = e_ns_get_node(0, id);
-		printf("\n!!!!!!!!!!! func %p\n",  func);
+		for(i = 0; i < PGlobalTaskMannager.count; i++)
+		{
+			if(PGlobalTaskMannager.tasks[i].id == id) 
+			{
+				printf("\n!!!!!!!!!!! already found == %u", id );
+				return;
+			}
+		}
+	/*	printf("\n!!!!!!!!!!! func %p\n",  func);
 		if(n != NULL)
 		{
 			printf("\n!!!!!!!!!!!adding type %u id %u func %p\n", e_ns_get_node_type(n), id, func);
-		}
+		}*/
 	}
 	if(PGlobalTaskMannager.count == P_MAX_TASKS)
 		return;
@@ -67,12 +76,13 @@ void p_task_compute(uint count)
 	{
 		if(PGlobalTaskMannager.count == 0)
 			return;
-		for(t = &PGlobalTaskMannager.tasks[PGlobalTaskMannager.current]; t->wait < 0.99; t = &PGlobalTaskMannager.tasks[PGlobalTaskMannager.current])
-		{
-			t->wait += t->importance;
+//		for(t = &PGlobalTaskMannager.tasks[PGlobalTaskMannager.current]; t->wait < 0.99; t = &PGlobalTaskMannager.tasks[PGlobalTaskMannager.current])
+//		{
+//			t->wait += t->importance;
 			PGlobalTaskMannager.current = (PGlobalTaskMannager.current + 1) % PGlobalTaskMannager.count;
-		}
-		t->wait = t->importance;
+//		}
+//		t->wait = t->importance;
+			t = &PGlobalTaskMannager.tasks[PGlobalTaskMannager.current];
 		if(t->func(t->id))
 		{
 			PGlobalTaskMannager.tasks[PGlobalTaskMannager.current] = PGlobalTaskMannager.tasks[--PGlobalTaskMannager.count];
@@ -88,13 +98,14 @@ extern void p_object_func(ENode *node, ECustomDataCommand command);
 extern void p_geometry_func(ENode *node, ECustomDataCommand command);
 extern void p_shader_func(ENode *node, ECustomDataCommand command);
 extern void p_texture_func(ENode *node, ECustomDataCommand command);
-
+extern void p_array_init();
 extern void p_env_init(uint size);
 
 void persuade_init(uint max_tesselation_level, void *(*gl_GetProcAddress)(const char* proc))
 {
 	uint i;
 	p_extention_init(gl_GetProcAddress);
+	p_array_init();
 	p_shader_init();
 	p_env_init(8);
 	PGlobalTaskMannager.count = 0;
@@ -114,6 +125,5 @@ void persuade_init(uint max_tesselation_level, void *(*gl_GetProcAddress)(const 
 
 	e_ns_set_custom_func(P_ENOUGH_SLOT, V_NT_OBJECT, p_object_func);
 	e_ns_set_custom_func(P_ENOUGH_SLOT, V_NT_GEOMETRY, p_geometry_func);
-	e_ns_set_custom_func(P_ENOUGH_SLOT, V_NT_MATERIAL, p_shader_func);
 	e_ns_set_custom_func(P_ENOUGH_SLOT, V_NT_BITMAP, p_texture_func);
 }
