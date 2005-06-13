d3d9e7a023b672be043bbc6ed983cafe8e3499da
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-06-13 11:21:48 +0000

    Mesh changes
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2598 49c17534-1d02-dc11-b283-0030488c597c

--- p_object.h
+++ p_object.h
@@ -1,6 +1,6 @@
 
 #define LIGHTS_PER_OBJECT 8
-
+/*
 typedef struct{
 	uint32	id;
 	uint32	material;
@@ -10,13 +10,20 @@ typedef struct{
 	PMesh	*progress;
 	void	*param;
 }PRenderSets;
+*/
 
 typedef struct{
 	uint32		node_id;
-	PRenderSets *sets;
-	uint		set_count;
-	PRenderSets temp;
 	uint32		lights[LIGHTS_PER_OBJECT];
+	float		lightfade;
+}PObjLight;
+
+typedef struct{
+	uint32		node_id;
+	PMesh		**meshes;
+	uint		mesh_count;
+	uint		version;
+	PObjLight	light;
 	void		*environment;
 	boolean		task;
 }PObject;
@@ -31,3 +38,21 @@ extern void *p_param_array_allocate(ENode *g_node, ENode *m_node);
 extern void p_param_array_destroy(void *p);
 extern boolean p_array_update(void *p, PMesh *mesh, ENode *g_node, ENode *m_node);
 extern egreal **p_param_get_array(void *p);
+
+/* handeling drawable meshas */
+
+void	p_rm_set_eay(PMesh *mesh, egreal *eay);
+void	p_rm_destroy(PMesh *mesh);
+PMesh	*p_rm_service(PMesh *mesh, ENode *o_node, const egreal *vertex);
+void	p_rm_unvalidate(PMesh *mesh);
+boolean p_rm_drawable(PMesh *mesh);
+uint	p_rm_get_geometry_node(PMesh *mesh);
+
+
+/* light */
+
+
+extern double	p_light_compute_brightnes(double *obj, uint32 node_id, uint32 time_s, uint32 time_f);
+extern void		p_light_update(PObjLight *light, uint32 node_id, uint light_count, uint32 time_s, uint32 time_f);
+extern void		p_set_light(PObjLight *light, uint light_count, uint32 time_s, uint32 time_f);
+
