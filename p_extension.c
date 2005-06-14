68f2bec1cc99da5e5eae9f6474c2060a8b2587a6
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-06-14 08:29:14 +0000

    OpenGL extension handling
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2619 49c17534-1d02-dc11-b283-0030488c597c

---
+++ p_extension.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef WIN32
+	#include <windows.h>
+	#include <GL/gl.h>
+#else
+	#include <GL/gl.h>
+#endif
+#include "verse.h"
+#include "persuade.h"
+#include "p_task.h"
+
+void *(*p_gl_GetProcAddress)(const char* proc) = NULL;
+
+void p_extention_init(void *(*gl_GetProcAddress)(const char* proc))
+{
+	p_gl_GetProcAddress = gl_GetProcAddress;
+}
+
+void *p_extention_get_address(const char* proc)
+{
+	return p_gl_GetProcAddress(proc);
+}
+
+boolean p_extention_test(char *string)
+{
+	const char *extension;
+	uint i;
+	char *a;
+
+	extension = glGetString(GL_EXTENSIONS);
+	printf("%s\n", extension);
+
+	for(a = extension; a != 0; a++)
+	{
+		for(i = 0; string[i] != 0 && a[i] != 0 && string[i] == a[i]; i++);
+		if(string[i] == 0)
+			return TRUE;
+	}
+
+	return FALSE;
+}
---
+++ p_extension.h
@@ -0,0 +1,4 @@
+
+extern void		p_extention_init(void *(*gl_GetProcAddress)(const char* proc));
+extern void		*p_extention_get_address(const char* proc);
+extern boolean	p_extention_test(char *string);
