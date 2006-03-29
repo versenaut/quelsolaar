c9f8425529bf1f1a7d354b0e2ee6bfd83cad6657
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-03-29 09:46:12 +0000

    EOL@EOF; spelling
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@3296 49c17534-1d02-dc11-b283-0030488c597c

--- co_summary.c
+++ co_summary.c
@@ -9,9 +9,9 @@
 
 #include "co_vn_handle.h"
 
-#define SUMARY_COLOR 0.5
+#define SUMMARY_COLOR 0.5
 
-float co_draw_node_sumary(ENode *node, float x, float y, uint recursion)
+static float co_draw_node_summary(ENode *node, float x, float y, uint recursion)
 {
 	char *type_name[] = {"object", "geometry", "material", "bitmap", "text", "curve", "audio"};
 	ENode *link_node;
@@ -22,21 +22,19 @@ float co_draw_node_sumary(ENode *node, float x, float y, uint recursion)
 	char text[2048];
 	EObjLink *link;
 
-
-	sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_ns_get_node_name(node), SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+	sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_ns_get_node_name(node), SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 	line -= 0.02;
-
 
 	content = FALSE;
 	for(i = e_ns_get_next_tag_group(node, 0); i != (uint16)-1; i = e_ns_get_next_tag_group(node, i + 1))
 	{
 		if(content == FALSE)
 		{
-			sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "Tags:", SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+			sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "Tags:", SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 			line -= 0.02;
 			content = TRUE;
 		}
-		sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_ns_get_tag_group(node, i), SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+		sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_ns_get_tag_group(node, i), SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 		line -= 0.02;
 		for(j = e_ns_get_next_tag(node, i, 0); j != (uint16)-1; j = e_ns_get_next_tag(node, i, j + 1))
 		{
@@ -72,18 +70,16 @@ float co_draw_node_sumary(ENode *node, float x, float y, uint recursion)
 						sprintf(text, "%s = size", e_ns_get_tag_name(node, i, j), tag->vblob.size);
 				break;
 			}
-			sui_draw_text(x + 0.04, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, text, SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+			sui_draw_text(x + 0.04, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, text, SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 			line -= 0.02;
 		}
 	}
-
-
 
 	switch(e_ns_get_node_type(node))
 	{
 		case V_NT_OBJECT :
 			{
-				sui_draw_text(0.2, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "Links:", SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+				sui_draw_text(0.2, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "Links:", SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 				line -= 0.02;
 				for(link = e_nso_get_next_link(node, 0); link != NULL; link = e_nso_get_next_link(node, e_nso_get_link_id(link) + 1))
 				{
@@ -91,10 +87,10 @@ float co_draw_node_sumary(ENode *node, float x, float y, uint recursion)
 					if((link_node = e_ns_get_node(0, e_nso_get_link_node(link))) != NULL)
 					{
 						sprintf(text, "lable: %s");
-						sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, text, SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+						sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, text, SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 						line -= 0.02;
 						if(recursion != 0)
-							line -= co_draw_node_sumary(link_node, x + 0.04, line, recursion - 1);
+							line -= co_draw_node_summary(link_node, x + 0.04, line, recursion - 1);
 					}
 				}
 
@@ -103,15 +99,15 @@ float co_draw_node_sumary(ENode *node, float x, float y, uint recursion)
 				{
 					if(content == FALSE)
 					{
-						sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "Methods:", SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+						sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "Methods:", SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 						line -= 0.02;
 						content = TRUE;
 					}
-					sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_nso_get_method_group(node, i), SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+					sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_nso_get_method_group(node, i), SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 					line -= 0.02;
 					for(j = e_nso_get_next_method(node, i, 0); j != (uint16)-1; j = e_nso_get_next_method(node, i, j + 1))
 					{
-						sui_draw_text(x + 0.04, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_nso_get_method(node, i, j), SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+						sui_draw_text(x + 0.04, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_nso_get_method(node, i, j), SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 						line -= 0.02;
 					}
 				}
@@ -125,11 +121,11 @@ float co_draw_node_sumary(ENode *node, float x, float y, uint recursion)
 				{
 					if(content == FALSE)
 					{
-						sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "layers:", SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+						sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "layers:", SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 						line -= 0.02;
 						content = TRUE;
 					}
-					sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_nsg_get_layer_name(layer), SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+					sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_nsg_get_layer_name(layer), SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 					line -= 0.02;
 				}
 			}
@@ -144,12 +140,12 @@ float co_draw_node_sumary(ENode *node, float x, float y, uint recursion)
 						VMatFrag *frag;
 						if(content == FALSE)
 						{
-							sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "Methods:", SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+							sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "Methods:", SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 							line -= 0.02;
 							content = TRUE;
 						}
 						frag = e_nsm_get_fragment(node, i);
-						sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, frag->output.label, SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+						sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, frag->output.label, SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 						line -= 0.02;
 					}
 				}
@@ -162,17 +158,17 @@ float co_draw_node_sumary(ENode *node, float x, float y, uint recursion)
 				content = FALSE;
 				e_nsb_get_size(node, &size[0], &size[1], &size[2]);
 				sprintf(text, "size: = %u * %u * %u", size[0], size[1], size[2]);
-				sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, text, SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+				sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, text, SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 				line -= 0.02;
 				for(layer = e_nsb_get_layer_next(node, 0); layer != NULL; layer = e_nsb_get_layer_next(node, e_nsb_get_layer_id(layer) + 1))
 				{
 					if(content == FALSE)
 					{
-						sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "layers:", SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+						sui_draw_text(x, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, "layers:", SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 						line -= 0.02;
 						content = TRUE;
 					}
-					sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_nsb_get_layer_name(layer), SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
+					sui_draw_text(x + 0.02, line, SUI_T_SIZE * 0.7, SUI_T_SPACE * 0.7, e_nsb_get_layer_name(layer), SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
 					line -= 0.02;
 				}
 			}
@@ -183,15 +179,13 @@ float co_draw_node_sumary(ENode *node, float x, float y, uint recursion)
 		break;
 	}
 	return line;
-
 }
 
 void co_draw_sumary(ENode *node, float x, float y)
 {
 	glPushMatrix();
 	glTranslatef(-x, -y, -1);
-	sui_draw_2d_line_gl(0.03, 0.015, 0.18, 0.09, SUMARY_COLOR, SUMARY_COLOR, SUMARY_COLOR);
-	co_draw_node_sumary(node, 0.2, 0.1, 10);
+	sui_draw_2d_line_gl(0.03, 0.015, 0.18, 0.09, SUMMARY_COLOR, SUMMARY_COLOR, SUMMARY_COLOR);
+	co_draw_node_summary(node, 0.2, 0.1, 10);
 	glPopMatrix();
-
 }
