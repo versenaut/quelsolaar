a2da7438ecc66d39041f436c15ca95accfd60f54
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-04-07 07:01:22 +0000

    More void
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@3319 49c17534-1d02-dc11-b283-0030488c597c

--- lp_layer_groups.h
+++ lp_layer_groups.h
@@ -1,5 +1,5 @@
 
-extern uint lp_layer_current_get();
+extern uint lp_layer_current_get(void);
 extern void lp_layer_current_set(uint group);
 
 extern EGeoLayer *lp_layer_get_red(uint group);
@@ -8,6 +8,6 @@ extern EGeoLayer *lp_layer_get_blue(uint group);
 
 extern char *lp_layer_get_name(uint group);
 
-extern uint lp_layer_get_group_count();
+extern uint lp_layer_get_group_count(void);
 
 extern void lp_update_layer_groups(ENode *node);
