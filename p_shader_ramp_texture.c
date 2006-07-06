32828b1b4f001214d083271fa99f5113f276ec0b
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-07-06 07:43:39 +0000

    Create texture for ramp material fragment
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@3576 49c17534-1d02-dc11-b283-0030488c597c

---
+++ p_shader_ramp_texture.c
@@ -0,0 +1,45 @@
+ #include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+	#include <windows.h>
+	#include <GL/gl.h>
+#else
+#if defined(__APPLE__) || defined(MACOSX)
+	#include <OpenGL/gl.h>
+#else
+	#include <GL/gl.h>
+#endif
+#endif
+#include "verse.h"
+#include "enough.h"
+#define GL_CLAMP_TO_EDGE                  0x812F
+
+uint p_shader_ramp_texture_create(uint texture_size, VMatFrag *frag)
+{
+	uint texture_id, i, j = 0;
+	float *buf, pos, scale, f;
+	glGenTextures(1, &texture_id);
+	glBindTexture(GL_TEXTURE_1D, texture_id);
+	glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	buf = malloc((sizeof *buf) * texture_size * 3);
+
+	pos = frag->ramp.ramp[0].pos;
+	scale = frag->ramp.ramp[frag->ramp.point_count - 1].pos - frag->ramp.ramp[0].pos;
+	for(i = 0; i < texture_size; i++)
+	{
+		f = (float)i / (float)texture_size;
+		f = pos + f * scale;
+		if(f >= frag->ramp.ramp[j + 1].pos && j + 1 < frag->ramp.point_count)
+			j++;
+		f = (f - frag->ramp.ramp[j].pos) / (frag->ramp.ramp[j + 1].pos - frag->ramp.ramp[j].pos);
+		buf[i * 3 + 0] = frag->ramp.ramp[j].red * (1.0 - f) + frag->ramp.ramp[j + 1].red * f;
+		buf[i * 3 + 1] = frag->ramp.ramp[j].green * (1.0 - f) + frag->ramp.ramp[j + 1].green * f;
+		buf[i * 3 + 2] = frag->ramp.ramp[j].blue * (1.0 - f) + frag->ramp.ramp[j + 1].blue * f;
+	}
+	glTexImage1D(GL_TEXTURE_1D, 0, GL_RGB, texture_size, 0, GL_RGB, GL_FLOAT, buf);
+	free(buf);
+	return texture_id;
+}
