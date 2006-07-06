7d677cbcb54c076b914ab1c2d1e843e815095fa0
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-07-06 07:03:14 +0000

    Settings
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@3565 49c17534-1d02-dc11-b283-0030488c597c

--- la_settings.c
+++ la_settings.c
@@ -2,6 +2,9 @@
 #include "la_includes.h"
 #include "la_geometry_undo.h"
 #include "la_projection.h"
+#include "la_draw_overlay.h"
+#include "la_particle_fx.h"
+
 
 extern void parse_input(BInputState *input, void *user);
 
@@ -26,105 +29,98 @@ void layer_name_func(void *user, char *text)
 
 void geometry_load_obj(void *user, char *file);
 void geometry_save_obj(void *user, char *file);
+void la_edit_func(BInputState *input, void *user);
+void la_draw_force_update_persuade(void);
 
-void draw_settings_menu(BInputState *input, void *user)
+void la_draw_settings_menu(BInputState *input, void *user)
 {
-    double a;
-	float pos[] = {-0.25, 0, 0};
-	static boolean complexity = FALSE, min = FALSE, max = FALSE, screen_mode = FALSE;
-	static uint choosen = 0, old = 0;
-	boolean active;
-
+	SUIViewElement element[10];
+	char *t, text[36];
+	uint i;
 	if(input->mode == BAM_MAIN)
 	{
 		verse_callback_update(0);
+#ifdef PERSUADE_H
+		p_task_compute(1);
+#endif
 		return;
 	}
+	element[0].type = S_VET_BOOLEAN;
+	element[0].text = "dispalay Flares";
+	element[0].param.checkbox = sui_get_setting_int("DISPLAY_SILLY_FLARES", TRUE);
+
+	element[1].type = S_VET_BOOLEAN;
+	element[1].text = "display SDS";
+	element[1].param.checkbox = sui_get_setting_int("RENDER_AS_SDS", TRUE);
+
+	element[2].type = S_VET_SLIDER;
+	element[2].text = "complexity";
+	element[2].param.slider = sqrt(sui_get_setting_double("GEOMETRY_COMPLEXITY", 1) / 1000.0);
+
+	element[3].type = S_VET_INTEGER;
+	element[3].text = "maximum Tesselation";
+	element[3].param.integer = sui_get_setting_int("MAX_TESS_LEVEL", 4);
+
+	element[4].type = S_VET_INTEGER;
+	element[4].text = "minimum Tesselation";
+	element[4].param.integer = sui_get_setting_int("MIN_TESS_LEVEL", 0);
+
+	element[5].type = S_VET_TEXT;
+	element[5].text = "author";
+	t = sui_get_setting_text("AUTHOR", "eskil");
+	for(i = 0; i < 32 && t[i] != 0; i++)
+		text[i] = t[i];
+	text[i] = 0;
+	element[5].param.text.text = text;
+	element[5].param.text.length = 32;
+
+	element[6].type = S_VET_INTEGER;
+	element[6].text = "revole section";
+	element[6].param.integer = sui_get_setting_int("REVOLVE_INTERSECTIONS", 8);
+
+	element[7].type = S_VET_INTEGER;
+	element[7].text = "undo buffer";
+	element[7].param.integer = sui_get_setting_int("UNDO_MEMORY_FOOT_PRINT", 5000000);
 
 	if(input->mode == BAM_DRAW)
 	{
+		glClearColor(0, 0, 0, 0);
+		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+		glPushMatrix();
+		p_view_set();
+		la_do_owerlay();
+		glPushMatrix();
+		la_pfx_select_vertex();
 		glPopMatrix();
 		glPopMatrix();
+		la_pfx_draw(FALSE);
+
 		glPushMatrix();
 		glTranslatef(0, 0, -1);
-		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-		glDisable(GL_DEPTH_TEST); 
-	    sw_draw_window(0, 0, 0.4, 0.6, 0.02);
-/*		sui_draw_text("SETTINGS", SUITM_CENTER_SPACED, NULL, NULL, 0, 0, 0.5 , 0, sui_get_material(SUIM_BLACK_FONT));*/
-		sui_draw_text(0 - 0.5 * sui_compute_text_length(SUI_T_SIZE, SUI_T_SPACE, "SETTINGS"), 0, SUI_T_SIZE, SUI_T_SPACE, "SETTINGS", 0, 0, 0);
-    }
-/*
-	sui_set_setting_int("DISPLAY_SILLY_FLARES", sw_symb_check_box(input, -0.25, 0.4, sui_get_setting_int("DISPLAY_SILLY_FLARES", TRUE)));
-	sui_draw_text("Dispaly Flares", SUITM_NORMAL, NULL, NULL, 0, -0.15, 0.4, 0, sui_get_material(SUIM_BLACK_FONT));
-
-	sui_set_setting_int("RENDER_AS_SDS",sw_symb_check_box(input, -0.25, 0.3, sui_get_setting_int("RENDER_AS_SDS", TRUE)));
-	sui_draw_text("Dispaly Subdivision", SUITM_NORMAL, NULL, NULL, 0, -0.15, 0.3, 0, sui_get_material(SUIM_BLACK_FONT));
-
-	sui_set_setting_int("WINDOW_FULLSCREEN",sw_symb_check_box(input, -0.25, 0.2, sui_get_setting_int("WINDOW_FULLSCREEN", FALSE)));
-	sui_draw_text("Fullscreen", SUITM_NORMAL, NULL, NULL, 0, -0.15, 0.2, 0, sui_get_material(SUIM_BLACK_FONT));
-
-	sui_set_setting_int("RENDER_AS_SDS",sw_symb_check_box(input, -0.25, 0.0, sui_get_setting_int("RENDER_AS_SDS", TRUE)));
-	sui_draw_text("Dispaly Subdivision", SUITM_NORMAL, NULL, NULL, 0, -0.15, 0.0, 0, sui_get_material(SUIM_BLACK_FONT));
-
-*/
-
-/*	boolean udg_update_geometry(void)*/
-
-/*	if(sw_text_button_center(input, SUIMC_BLACK, 0, -0.5, "OK"))
+		glDisable(GL_DEPTH_TEST);
+	}
+	if(sui_draw_setting_view(input, 0, 0, 0.5, element, 8, "settings", 0.0))
+	{
+		sui_save_settings("la_config.cfg");
 		betray_set_action_func(la_edit_func, NULL);
-*/
-/*	if(udg_update_geometry())
-		betray_set_action_func(parse_input, NULL);
-*/
-/*
-	sui_draw_text("Geometry complexity", SUITM_NORMAL, NULL, NULL, 0, pos[0] + 0.1, -0.1 , 0, sui_get_material(SUIM_BLACK_FONT));
-	sui_draw_text("Min Tesselation", SUITM_NORMAL, NULL, NULL, 0, pos[0] + 0.1, -0.2 , 0, sui_get_material(SUIM_BLACK_FONT));
-	sui_draw_text("Max Tesselation", SUITM_NORMAL, NULL, NULL, 0, pos[0] + 0.1, -0.3 , 0, sui_get_material(SUIM_BLACK_FONT));
-
-	a = sui_get_setting_double("GEOMETRY_COMPLEXITY", 40) / 100;
-	pos[1] = -0.1;
-	sw_draw_background_point(pos);
-	if(complexity = sw_dir_slider_double_alt(input, complexity, 8, pos, &a))
-		sui_set_setting_double("GEOMETRY_COMPLEXITY", a * 100);
-	pos[1] = -0.2;
-	a = ((double)sui_get_setting_int("MIN_TESS_LEVEL", 1) - 1.0) / 5.0;
-	sw_draw_background_point(pos);
-	if(min = sw_dir_slider_double_alt(input, min, 5, pos, &a))
-		sui_set_setting_int("MIN_TESS_LEVEL", (double)(a * 5.0 + 1.0));
-	pos[1] = -0.3;
-	a = ((double)sui_get_setting_int("MAX_TESS_LEVEL", 1) - 1.0) / 5.0;
-	sw_draw_background_point(pos);
-	if(max = sw_dir_slider_double_alt(input, max, 5, pos, &a))
-		sui_set_setting_int("MAX_TESS_LEVEL", (double)(a * 5.0 + 1.0));
-*/
-/*	void done_func(void *user)*/
-/*
-		static boolean layer_mode = FALSE;
-		EGeoLayer *layer;
-		char **names;
-		uint count = 0;
-		ENode *node;
-		node = e_ns_get_node(0, udg_get_modeling_node());
-
-		for(layer = e_nsg_get_layer_next(node, 0); layer != NULL; layer = e_nsg_get_layer_next(node, e_nsg_get_layer_id(layer) + 1))
-			if(VN_G_LAYER_VERTEX_XYZ == e_nsg_get_layer_type(layer))
-				count++;
-		names = malloc((sizeof *names) * count);
-		count = 0;
-		for(layer = e_nsg_get_layer_next(node, 0);layer != NULL; layer = e_nsg_get_layer_next(node, e_nsg_get_layer_id(layer) + 1))
-			if(VN_G_LAYER_VERTEX_XYZ == e_nsg_get_layer_type(layer))
-				names[count++] = e_nsg_get_layer_name(layer);
-		layer_mode = sw_type_in_pulldown(input, -0.25, 0.1, 0.5, "Nisse", 15, names, count, layer_name_func, NULL, layer_mode);
-	}*/
-
-/*	sui_type_in(input, -0.25, 0.2, 0.5, "load.obj", 32, geometry_load_obj, NULL);
-	sui_type_in(input, -0.25, 0.3, 0.5, "save.obj", 32, geometry_save_obj, NULL);
-*/
+	}
 	if(input->mode == BAM_DRAW)
 	{
-		glEnable(GL_DEPTH_TEST); 
-		glPushMatrix();
-		glPushMatrix();
-		glPopMatrix();  
-    }	
+		glPopMatrix();
+		glEnable(GL_DEPTH_TEST);
+	}
+	sui_set_setting_int("DISPLAY_SILLY_FLARES",		element[0].param.checkbox);
+	sui_set_setting_int("RENDER_AS_SDS",			element[1].param.checkbox);
+	sui_set_setting_double("GEOMETRY_COMPLEXITY",	element[2].param.slider * element[2].param.slider * 1000.0);
+	sui_set_setting_int("MAX_TESS_LEVEL",			element[3].param.integer);
+	sui_set_setting_int("MIN_TESS_LEVEL",			element[4].param.integer);
+	sui_set_setting_text("AUTHOR",					element[5].param.text.text);
+	sui_set_setting_int("REVOLVE_INTERSECTIONS",	element[6].param.integer);
+	sui_set_setting_int("UNDO_MEMORY_FOOT_PRINT",	element[7].param.integer);
+
+
+	p_geo_set_sds_level(element[3].param.integer);
+	p_geo_set_sds_force_level(element[4].param.integer);
+	p_geo_set_sds_mesh_factor(element[2].param.slider * element[2].param.slider * 1000.0);
+	la_draw_force_update_persuade();
 }
