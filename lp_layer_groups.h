c947b283f046f83ced08078057d38f5f79ff457b
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-10-18 15:26:00 +0000

    Re-added lp_layer_current_is_integer()
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@3984 49c17534-1d02-dc11-b283-0030488c597c

--- lp_layer_groups.h
+++ lp_layer_groups.h
@@ -6,6 +6,8 @@ extern EGeoLayer *lp_layer_get_red(uint group);
 extern EGeoLayer *lp_layer_get_green(uint group);
 extern EGeoLayer *lp_layer_get_blue(uint group);
 
+extern boolean lp_layer_current_is_integer(void);
+
 extern char *lp_layer_get_name(uint group);
 
 extern uint lp_layer_get_group_count(void);
