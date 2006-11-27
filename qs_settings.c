923c267bc2efacca0a381c48fb7e7e59682748ca
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-11-27 10:15:22 +0000

    New settings
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4095 49c17534-1d02-dc11-b283-0030488c597c

--- qs_settings.c
+++ qs_settings.c
@@ -20,19 +20,19 @@ void sp_settings_post(SUIViewElement *element);
 
 boolean qs_draw_settings(BInputState *input)
 {
-	SUIViewElement element[14];
+	SUIViewElement element[15];
 	boolean output;
 	char text[32] = {0};
 	sp_settings_pre(element);
-	element[13].type = S_VET_COLOR;
-	element[13].text = "background";
-	element[13].param.color[0] = background_color[0];
-	element[13].param.color[1] = background_color[1];
-	element[13].param.color[2] = background_color[2];
-	output = !sui_draw_setting_view(input, 0, 0.3, 0.5, element, 14, "SETTINGS", 0.0);
+	element[14].type = S_VET_COLOR;
+	element[14].text = "background";
+	element[14].param.color[0] = background_color[0];
+	element[14].param.color[1] = background_color[1];
+	element[14].param.color[2] = background_color[2];
+	output = !sui_draw_setting_view(input, 0, 0.3, 0.5, element, 15, "SETTINGS", 0.0);
 	sp_settings_post(element);
-	background_color[0] = element[13].param.color[0];
-	background_color[1] = element[13].param.color[1];
-	background_color[2] = element[13].param.color[2];
+	background_color[0] = element[14].param.color[0];
+	background_color[1] = element[14].param.color[1];
+	background_color[2] = element[14].param.color[2];
 	return output;
 }
