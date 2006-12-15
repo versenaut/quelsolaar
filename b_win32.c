421014a740685ab1787b157fabe7283f2b154f6d
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-12-15 08:12:12 +0000

    Bunches of fixes, this builds now (using Makefile, at least)
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4141 49c17534-1d02-dc11-b283-0030488c597c

--- b_win32.c
+++ b_win32.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 #include "st_types.h"
 #include "betray.h"
 
@@ -14,24 +15,26 @@ static boolean	input_focus = TRUE;
 static uint	screen_size_x = 800;
 static uint	screen_size_y = 600;
 static boolean	mouse_warp = FALSE;
-static HDC	hDC;				/* device context */
 static HWND	hWnd;
+static HDC	hDC;				/* device context */
+static boolean	busy;
+static void	(*win32_context_func)(void) = NULL;
+static int	my_nCmdShow = SW_SHOW;
+static HGLRC	hRC = NULL;			/* opengl context */
 
 extern void sui_get_input(BInputState *data);
 
 boolean b_win32_system_wrapper_set_display(uint size_x, uint size_y, boolean full_screen) 
-{  
+{
 	return TRUE;
 }
 
-void (*sdl_context_func)(void) = NULL;
-
 void betray_set_context_update_func(void (*context_func)(void))
 {
-	sdl_context_func = context_func;
+	win32_context_func = context_func;
 }
 
-LONG WINAPI WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+static LONG WINAPI WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 { 
 	static PAINTSTRUCT ps;
 	static int omx, omy, mx, my;
@@ -41,102 +44,85 @@ LONG WINAPI WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	input = betray_get_input_state();
 	switch(uMsg)
 	{
-		case WM_PAINT :
+		case WM_PAINT:
 			betray_action(BAM_DRAW);
 			glFlush();
 			SwapBuffers(hDC);
 			BeginPaint(hWnd, &ps);
 			EndPaint(hWnd, &ps);
-		return 0;
-		case WM_SIZE :
+			return 0;
+		case WM_SIZE:
 			screen_size_x = LOWORD(lParam);
 			screen_size_y = HIWORD(lParam);
 			betray_reshape_view(screen_size_x, screen_size_y);
-		return 0;
-	    case WM_LBUTTONDOWN :
-	    case WM_LBUTTONUP :
-	    case WM_RBUTTONDOWN :
-	    case WM_RBUTTONUP :
-	    case WM_MBUTTONDOWN :
-	    case WM_MBUTTONUP :
-	    case WM_MOUSEMOVE :
-		if(uMsg == WM_LBUTTONDOWN)
-			input->mouse_button[0] = TRUE;
-		if(uMsg == WM_LBUTTONUP)
-			input->mouse_button[0] = FALSE;
-		if(uMsg == WM_RBUTTONDOWN)
-			input->mouse_button[1] = TRUE;
-		if(uMsg == WM_RBUTTONUP)
-			input->mouse_button[1] = FALSE;
-		if(uMsg == WM_MBUTTONDOWN)
-			input->mouse_button[2] = TRUE;
-		if(uMsg == WM_MBUTTONUP)
-			input->mouse_button[2] = FALSE;
-
-		input->pointer_x = (float)LOWORD(lParam) / (float)screen_size_x * 2.0 - 1.0;
-		input->pointer_y = (1.0 - (float)HIWORD(lParam) / (float)screen_size_y * 2.0) * (float)screen_size_y / (float)screen_size_x;
-		{
-			RECT r;
-			int		style;
-			r.left   = 0;
-			r.top    = 0;
-			r.right  = 1;
-			r.bottom = 1;
-
-			style = GetWindowLong( hWnd, GWL_STYLE );
-			AdjustWindowRect( &r, style, FALSE );
-		}
-		return 0;
+			return 0;
+		case WM_LBUTTONDOWN:
+		case WM_LBUTTONUP:
+		case WM_RBUTTONDOWN:
+		case WM_RBUTTONUP:
+		case WM_MBUTTONDOWN:
+		case WM_MBUTTONUP:
+		case WM_MOUSEMOVE:
+			if(uMsg == WM_LBUTTONDOWN)
+				input->mouse_button[0] = TRUE;
+			else if(uMsg == WM_LBUTTONUP)
+				input->mouse_button[0] = FALSE;
+			else if(uMsg == WM_RBUTTONDOWN)
+				input->mouse_button[1] = TRUE;
+			else if(uMsg == WM_RBUTTONUP)
+				input->mouse_button[1] = FALSE;
+			else if(uMsg == WM_MBUTTONDOWN)
+				input->mouse_button[2] = TRUE;
+			else if(uMsg == WM_MBUTTONUP)
+				input->mouse_button[2] = FALSE;
+			input->pointer_x = (float)LOWORD(lParam) / (float)screen_size_x * 2.0f - 1.0f;
+			input->pointer_y = (1.0f - (float)HIWORD(lParam) / (float)screen_size_y * 2.0f) * (float)screen_size_y / (float)screen_size_x;
+			return 0;
 		case WM_SYSKEYDOWN:
 		case WM_KEYDOWN:
-				{
-					static FILE *f = NULL;
-					f = fopen("key_output.txt", "a");
-					fprintf(f, "sys pressed = %u %u\n", wParam, lParam); 
-					fclose(f);
-				}
-				if(betray_is_type_in())
-				{
-					if(wParam == 8) /* enter */
-					{
-					//	betray_move_cursor(1);
-						betray_delete_character();
-					}
-					else if(wParam == 13) /* enter */
-						betray_end_type_in_mode(FALSE);
-					else if(wParam == 27) /* ESC */
-						betray_end_type_in_mode(TRUE);
-					else if(wParam == 39)
-						betray_move_cursor(1);
-					else if(wParam == 37)
-						betray_move_cursor(-1);
-					else if(wParam == 46)
-					{
-						betray_move_cursor(1);
-						betray_delete_character();
-					}
-					else if(wParam == 35) /* END */
-						betray_move_cursor(32000);
-					else if(wParam == 36) /* HOME */
-						betray_move_cursor(-32000);
-				}
-			break;
-		case WM_CHAR :
+/*			{
+				FILE *f;
+				f = fopen("key_output.txt", "at");
+				fprintf(f, "keydown pressed = %u %u\n", wParam, lParam); 
+				fclose(f);
+			}
+*/			if(betray_is_type_in())
 			{
-				uint key;
-			/*	{
-					static FILE *f = NULL;
-					f = fopen("key_output.txt", "a");
-					fprintf(f, "key pressed = %u\n", wParam); 
+				if(wParam == 8) /* enter */
+					betray_delete_character();
+				else if(wParam == 13) /* enter */
+					betray_end_type_in_mode(FALSE);
+				else if(wParam == 27) /* ESC */
+					betray_end_type_in_mode(TRUE);
+				else if(wParam == 39)
+					betray_move_cursor(1);
+				else if(wParam == 37)
+					betray_move_cursor(-1);
+				else if(wParam == 46)
+				{
+					betray_move_cursor(1);
+					betray_delete_character();
+				}
+				else if(wParam == 35) /* END */
+					betray_move_cursor(32000);
+				else if(wParam == 36) /* HOME */
+					betray_move_cursor(-32000);
+			}
+			break;
+		case WM_CHAR:
+			{
+				uint key = wParam;
+/*				{
+					FILE *f;
+					f = fopen("key_output.txt", "at");
+					fprintf(f, "char pressed = %u\n", wParam); 
 					fclose(f);
-				}*/
-				key = wParam;
+				}
+*/
 				if(betray_is_type_in())
 				{
-					if(key > 32 && key < 128)
-					{
+					if(key >= ' ' && key < 128)
 						betray_insert_character(key);
-					}
 				}
 				else
 				{
@@ -150,27 +136,22 @@ LONG WINAPI WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			break;
 		case WM_SYSKEYUP:
 		case WM_KEYUP:
+			if(!betray_is_type_in() && input->event_count < MAX_EVENT_COUNT)
 			{
-				if(!betray_is_type_in() && input->event_count < MAX_EVENT_COUNT)
-				{
-					input->event[input->event_count].state = FALSE;
-					input->event[input->event_count++].button = lParam;
-				}
+				input->event[input->event_count].state = FALSE;
+				input->event[input->event_count++].button = lParam;
 			}
 			break;
-	    case WM_CLOSE:
-			exit(0);
+		case WM_CLOSE:
+			busy = 0;
+			break;
 	}
 	return DefWindowProc(hWnd, uMsg, wParam, lParam); 
 }
 
-
-HWND  hWnd;				/* window */
-
 boolean b_win32_init_display(uint size_x, uint size_y, boolean full_screen, const char *caption) 
 {
 	int n, pf;
-	HGLRC hRC;				/* opengl context */
 	WNDCLASS    wc;
 	LOGPALETTE* lpPal;
 	PIXELFORMATDESCRIPTOR pfd;
@@ -193,7 +174,7 @@ boolean b_win32_init_display(uint size_x, uint size_y, boolean full_screen, cons
 
 		if(!RegisterClass(&wc))
 		{
-			printf("Betray: RegisterClass() failed: Cannot register window class.\n");
+			fprintf(stderr, "Betray: RegisterClass() failed: Cannot register window class.\n");
 			return FALSE;
 		}
 	}
@@ -210,8 +191,8 @@ boolean b_win32_init_display(uint size_x, uint size_y, boolean full_screen, cons
 
 	/* there is no guarantee that the contents of the stack that become
 	the pfd are zeroed, therefore _make sure_ to clear these bits. */
-	memset(&pfd, 0, sizeof(pfd));
-	pfd.nSize = sizeof(pfd);
+	memset(&pfd, 0, sizeof pfd);
+	pfd.nSize = sizeof pfd;
 	pfd.nVersion = 1;
 	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
 	pfd.iPixelType = PFD_TYPE_RGBA;
@@ -231,8 +212,8 @@ boolean b_win32_init_display(uint size_x, uint size_y, boolean full_screen, cons
 		return FALSE;
 	} 
 
-	DescribePixelFormat(hDC, pf, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
-	ReleaseDC(hDC, hWnd);
+	DescribePixelFormat(hDC, pf, sizeof pfd, &pfd);
+	ReleaseDC(hWnd, hDC);
 
 	hDC = GetDC(hWnd);
 	hRC = wglCreateContext(hDC);
@@ -257,49 +238,40 @@ void betray_set_mouse_warp(boolean warp)
 
 //PROC  ( WINAPI * bglGetProcAddress)(LPCSTR);
 
-void *betray_get_gl_proc_address()
+/* If we return wglGetProcAddress() directly, it doesn't work. Trampoline it. */
+static void * get_proc_address(LPCSTR name)
 {
-	HINSTANCE hinstOpenGL;
-//	return &SDL_GL_GetProcAddress;
-//	hinstOpenGL = LoadLibrary("opengl32");
-//	bglGetProcAddress = GetProcAddress(hinstOpenGL, "wglGetProcAddress");
-	return &wglGetProcAddress;
+	void	*f = wglGetProcAddress(name);
+//	printf("looking up '%s', got %p\n", name, f);
+	return f;
+}
+
+void * betray_get_gl_proc_address()
+{
+	return get_proc_address;
 }
 
 extern void betray_time_update(void);
 
-int my_nCmdShow;
-
 void betray_launch_main_loop(void)
 {
-	MSG   msg;				/* message */
-	BInputState *input;
+	MSG		msg;				/* message */
+	BInputState	*input;
 
 	input = betray_get_input_state();
 	ShowWindow(hWnd, my_nCmdShow);
-
-/*	while(GetMessage(&msg, hWnd, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	while(GetMessage(&msg, NULL, 0, 0))
+	busy = TRUE;
+	while(busy)
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-*/
-	while(TRUE)
-	{
-		float x, y;
 		input->delta_pointer_x = -input->pointer_x;
 		input->delta_pointer_y = -input->pointer_y;
 		while(PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
 		{
-			if(!GetMessage(&msg, NULL, 0, 0))
-				;
-			TranslateMessage(&msg);
-   			DispatchMessage(&msg);
+			if(GetMessage(&msg, NULL, 0, 0) > 0)
+			{
+				TranslateMessage(&msg);
+   				DispatchMessage(&msg);
+   			}
 		}
 		input->delta_pointer_x += input->pointer_x;
 		input->delta_pointer_y += input->pointer_y;
@@ -323,16 +295,22 @@ void betray_launch_main_loop(void)
 		input->last_mouse_button[2] = input->mouse_button[2];
 		betray_action(BAM_MAIN);
 	}
+	glFlush();
+	fprintf(stderr, "deleting context\n");
+	wglDeleteContext(hRC);
+	fprintf(stderr, "destroying window\n");
+	DestroyWindow(hWnd);
 }
 
-extern int main(int argc, char **argv);
+extern int main(int argc, char *argv[]);
 
+/* Entrypoint for Windows native application. Not used for console build. */
 int APIENTRY WinMain(HINSTANCE hCurrentInst, HINSTANCE hPreviousInst, LPSTR lpszCmdLine, int nCmdShow)
 {
 	char *argv[] = { "file.exe", NULL };
 
 	my_nCmdShow = nCmdShow;
-	main(1, &argument);
+	main(1, argv);
 
 	return TRUE;
 }
