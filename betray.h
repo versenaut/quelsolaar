04837afa3cefc101e57549d724d71c1514d29dda
Emil Brisk <emil.brink@interactiveinstitute.se>
2007-01-11 08:48:18 +0000

    New internal API added, minor whitespace cleaning (TABs are 8)
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4280 49c17534-1d02-dc11-b283-0030488c597c

--- betray.h
+++ betray.h
@@ -1,3 +1,6 @@
+/*
+ * This is the header for the Betray windowing system abstraction layer. 
+*/
 
 #if !defined(BETRAY_H)
 #define	BETRAY_H
@@ -20,10 +23,10 @@
 */
 #if defined _WIN32
 #define	BETRAY_WIN32_SYSTEM_WRAPPER
-#elif defined __linux__
+/*#elif defined __linux__
 #define	BETRAY_X11_SYSTEM_WRAPPER
-#else
-#define	BETRAY_SDL_SYSTEM_WRAPPER
+*/#else
+#define	BETRAY_GLFW_SYSTEM_WRAPPER
 #endif
 
 
@@ -79,31 +82,37 @@
 #define BETRAY_MAX_EVENT_COUNT		64
 #define BETRAY_BUTTONS_COUNT		(256 + 16) 
 
-typedef enum{
+typedef enum {
 	BAM_DRAW,
 	BAM_EVENT,
 	BAM_MAIN
-}BActionMode;
+} BActionMode;
 
-typedef struct{
+typedef struct {
 	uint	button;
 	boolean state;
-}BButtonEvent;
+} BButtonEvent;
 
-typedef struct{
-	boolean			mouse_button[BETRAY_DEVICE_BUTTONS_COUNT];
-	boolean			last_mouse_button[BETRAY_DEVICE_BUTTONS_COUNT];
-	float			pointer_x;
-	float			pointer_y;
-	float			click_pointer_x;
-	float			click_pointer_y;
-	float			delta_pointer_x;
-	float			delta_pointer_y;
+typedef struct {
+	boolean		mouse_button[BETRAY_DEVICE_BUTTONS_COUNT];
+	boolean		last_mouse_button[BETRAY_DEVICE_BUTTONS_COUNT];
+	float		pointer_x;
+	float		pointer_y;
+	float		click_pointer_x;
+	float		click_pointer_y;
+	float		delta_pointer_x;
+	float		delta_pointer_y;
 	BButtonEvent	event[16];
-	uint			event_count;
-	double			time;
-	BActionMode		mode;
-}BInputState;
+	uint		event_count;
+	double		time;
+	BActionMode	mode;
+} BInputState;
+
+/* Internal functions, not to be used directly by applications. */
+extern boolean		betray_internal_init_display(int argc, char **argv, uint size_x, uint size_y, boolean full_screen, const char *caption);
+extern boolean		betray_internal_set_display(uint x_size, uint y_size, boolean fullscreen);
+extern boolean		betray_internal_key_add(BInputState *input, uint key, boolean pressed);
+/* End of internal functions, below is public API. */
 
 extern void		betray_init(int argc, char **argv, uint window_size_x, uint window_size_y, boolean window_fullscreen, const char *name);
 extern void		betray_action(BActionMode mode);
@@ -111,36 +120,32 @@ extern void		betray_set_action_func(void (*input_compute_func)(BInputState *data
 extern void		betray_set_context_update_func(void (*context_func)(void));
 extern void		betray_set_mouse_warp(boolean warp);
 extern void		betray_execute(const char *command);
-extern float	betray_get_time(void);
-extern void		*betray_get_gl_proc_address(void);
+extern float		betray_get_time(void);
+extern void *		betray_get_gl_proc_address(void);
 
-extern boolean	betray_get_key(uint key);
+extern boolean		betray_get_key(uint key);
 extern void		betray_get_key_up_down(boolean *press, boolean *last_press, uint key);
 
-
-
-extern BInputState *betray_get_input_state(void);
+extern BInputState *	betray_get_input_state(void);
 
 /* To set and get the screen mode, please note that sui_get_screen_mode returns the aspect (width * aspect == height). */
 
 extern void		betray_reshape_view(uint x_size, uint y_size);
 
-extern boolean	betray_set_screen_mode(uint x_size, uint y_size, boolean fullscreen);
-extern double	betray_get_screen_mode(uint *x_size, uint *y_size, boolean *fullscreen);
-
+extern boolean		betray_set_screen_mode(uint x_size, uint y_size, boolean fullscreen);
+extern double		betray_get_screen_mode(uint *x_size, uint *y_size, boolean *fullscreen);
 
 extern void		betray_start_type_in(char *text, uint size, void (*done_func)(void *user, boolean cancel), uint *cursor, void *user_pointer);
 extern void		betray_end_type_in_mode(boolean cancel);
-
-extern boolean	betray_is_type_in(void);
+extern boolean		betray_is_type_in(void);
 extern void		betray_move_cursor(int move);
 extern void		betray_insert_character(char character);
 extern void		betray_delete_character(void);
 
-extern char		*betray_debug(void);
+extern char *		betray_debug(void);
 extern void		betray_launch_main_loop(void);
 
 extern void		betray_get_current_time(uint32 *seconds, uint32 *fractions);
-extern double	betray_get_delta_time(void);
+extern double		betray_get_delta_time(void);
 
 #endif
