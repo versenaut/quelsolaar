aa0887710b7bb2182188681c4ea5b77c79a632b5
Emil Brisk <emil.brink@interactiveinstitute.se>
2007-01-23 17:51:46 +0000

    Outdent; use memcpy(); displacement (XYZ) painting support shrinking
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4438 49c17534-1d02-dc11-b283-0030488c597c

--- lp_paint.c
+++ lp_paint.c
@@ -1,4 +1,7 @@
+
 #include <math.h>
+#include <string.h>
+
 #include "st_matrix_operations.h"
 #include "enough.h"
 #include "seduce.h"
@@ -140,7 +143,7 @@ void lp_normal_create(ENode *node)
 
 void lp_lock_paint(ENode *node, EGeoLayer *red, EGeoLayer *green, EGeoLayer *blue)
 {
-	EGeoLayer *(layers[3]);
+	EGeoLayer *layers[3];
 	boolean v = FALSE, p = FALSE;
 	egreal *real;
 	uint i, j, ref_length, length, *integer;
@@ -152,14 +155,13 @@ void lp_lock_paint(ENode *node, EGeoLayer *red, EGeoLayer *green, EGeoLayer *blu
 	layers[1] = green;
 	layers[2] = blue;
 
-
 	for(i = 0; i < 3; i++)
 	{
 		if(layers[i] != NULL)
 		{
 			switch(e_nsg_get_layer_type(layers[i]))
 			{
-				case VN_G_LAYER_VERTEX_XYZ :
+			case VN_G_LAYER_VERTEX_XYZ :
 				lp_normal_create(node);
 				real = e_nsg_get_layer_data(node, layers[i]);
 				lp_channels[i] = malloc(sizeof(egreal) * 3 * length);
@@ -167,49 +169,49 @@ void lp_lock_paint(ENode *node, EGeoLayer *red, EGeoLayer *green, EGeoLayer *blu
 					((egreal *)lp_channels[i])[j] = real[j];
 				v = TRUE;
 				break;
-				case VN_G_LAYER_VERTEX_UINT32 :
+			case VN_G_LAYER_VERTEX_UINT32 :
 				integer = e_nsg_get_layer_data(node, layers[i]);
 				lp_channels[i] = malloc(sizeof(uint) * length);
 				for(j = 0; j < length; j++)
 					((uint *)lp_channels[i])[j] = integer[j];
 				v = TRUE;
 				break;
-				case VN_G_LAYER_VERTEX_REAL :
+			case VN_G_LAYER_VERTEX_REAL :
 				real = e_nsg_get_layer_data(node, layers[i]);
 				lp_channels[i] = malloc(sizeof(egreal) * length);
 				for(j = 0; j < length; j++)
 					((egreal *)lp_channels[i])[j] = real[j];
 				v = TRUE;
 				break;
-				case VN_G_LAYER_POLYGON_CORNER_UINT32 :
+			case VN_G_LAYER_POLYGON_CORNER_UINT32 :
 				integer = e_nsg_get_layer_data(node, layers[i]);
 				lp_channels[i] = malloc(sizeof(uint32) * 4 * ref_length);
 				for(j = 0; j < ref_length * 4; j++)
 					((uint *)lp_channels[i])[j] = integer[j];
 				v = TRUE;
 				break;
-				case VN_G_LAYER_POLYGON_CORNER_REAL :
+			case VN_G_LAYER_POLYGON_CORNER_REAL :
 				real = e_nsg_get_layer_data(node, layers[i]);
 				lp_channels[i] = malloc(sizeof(egreal) * 4 * ref_length);
 				for(j = 0; j < ref_length * 4; j++)
 					((egreal *)lp_channels[i])[j] = real[j];
 				v = TRUE;
 				break;
-				case VN_G_LAYER_POLYGON_FACE_UINT8 :
+			case VN_G_LAYER_POLYGON_FACE_UINT8 :
 				integer8 = e_nsg_get_layer_data(node, layers[i]);
 				lp_channels[i] = malloc(sizeof(uint8) * ref_length);
 				for(j = 0; j < ref_length; j++)
 					((uint8 *)lp_channels[i])[j] = integer8[j];
 				p = TRUE;
 				break;
-				case VN_G_LAYER_POLYGON_FACE_UINT32 :
+			case VN_G_LAYER_POLYGON_FACE_UINT32 :
 				integer = e_nsg_get_layer_data(node, layers[i]);
 				lp_channels[i] = malloc(sizeof(uint32) * ref_length);
 				for(j = 0; j < ref_length; j++)
 					((uint *)lp_channels[i])[j] = integer[j];
 				p = TRUE;
 				break;
-				case VN_G_LAYER_POLYGON_FACE_REAL :
+			case VN_G_LAYER_POLYGON_FACE_REAL :
 				real = e_nsg_get_layer_data(node, layers[i]);
 				lp_channels[i] = malloc(sizeof(egreal) * ref_length);
 				for(j = 0; j < ref_length; j++)
@@ -325,131 +327,127 @@ void lp_apply_paint(ENode *node, EGeoLayer *red, EGeoLayer *green, EGeoLayer *bl
 	printf("blue - %s\n", e_nsg_get_layer_name(blue));
 */	for(i = 0; i < 3; i++)
 	{
-/*		printf("layers[%u] %p\n", i, layers[i]);*/
-		if(layers[i] != NULL)
+/*		printf(" layers[%u] %p\n", i, layers[i]);*/
+		if(layers[i] == NULL)
+			continue;
+		layer_id = e_nsg_get_layer_id(layers[i]);
+/*		printf("  layer %u: %u\n", i, e_nsg_get_layer_id(layers[i]));*/
+		switch(e_nsg_get_layer_type(layers[i]))
 		{
-			layer_id = e_nsg_get_layer_id(layers[i]);
-/*			printf("layers[%u] %u\n", i, e_nsg_get_layer_id(layers[i]));*/
-			switch(e_nsg_get_layer_type(layers[i]))
+			case VN_G_LAYER_VERTEX_XYZ :
+			for(j = 0; j < length; j++)
 			{
-				case VN_G_LAYER_VERTEX_XYZ :
-				for(j = 0; j < length; j++)
+				if(lp_vertex_influence_back[j] < lp_vertex_influence[j])
 				{
-					if(lp_vertex_influence_back[j] < lp_vertex_influence[j])
-					{
-						verse_send_g_vertex_set_xyz_real64(node_id, layer_id, j,
-										   ((egreal *)lp_channels[i])[j * 3] + lp_vertex_influence[j] * lp_normal[j * 3] * value[i],
-										   ((egreal *)lp_channels[i])[j * 3 + 1] + lp_vertex_influence[j] * lp_normal[j * 3 + 1] * value[i],
-										   ((egreal *)lp_channels[i])[j * 3 + 2] + lp_vertex_influence[j] * lp_normal[j * 3 + 2] * value[i]);
-					}
+/*					printf("painting XYZ, current is (%g,%g,%g), i=%u\n", value[0], value[1], value[2], i);*/
+					verse_send_g_vertex_set_xyz_real64(node_id, layer_id, j,
+									   ((egreal *)lp_channels[i])[j * 3 + 0] + lp_vertex_influence[j] * lp_normal[j * 3 + 0] * (value[i] - 0.5f),
+									   ((egreal *)lp_channels[i])[j * 3 + 1] + lp_vertex_influence[j] * lp_normal[j * 3 + 1] * (value[i] - 0.5f),
+									   ((egreal *)lp_channels[i])[j * 3 + 2] + lp_vertex_influence[j] * lp_normal[j * 3 + 2] * (value[i] - 0.5f));
 				}
-				break;
-				case VN_G_LAYER_VERTEX_UINT32 :
-				for(j = 0; j < length; j++)
-					if(lp_vertex_influence_back[j] < lp_vertex_influence[j] && ((uint32 *)lp_channels[i])[j] != integer)
-						verse_send_g_vertex_set_uint32(node_id, layer_id, j, integer);
-				break;
-				case VN_G_LAYER_VERTEX_REAL :
-				for(j = 0; j < length; j++)
-					if(lp_vertex_influence_back[j] < lp_vertex_influence[j])
-						verse_send_g_vertex_set_real64(node_id, layer_id, j, ((egreal *)lp_channels[i])[j] * (1.0 - lp_vertex_influence[j]) + value[i] * lp_vertex_influence[j]);
-				break;
-				case VN_G_LAYER_POLYGON_CORNER_UINT32 :
-				for(j = 0; j < ref_length; j++)
-				{
-					r[0] = ref[j * 4];
-					r[1] = ref[j * 4 + 1];
-					r[2] = ref[j * 4 + 2];
-					r[3] = ref[j * 4 + 3];
-					if(r[0] < length && r[1] < length && r[2] < length)
-					{				
-						if(0 < lp_vertex_influence[r[0]] ||
-						   0 < lp_vertex_influence[r[1]] ||
-						   0 < lp_vertex_influence[r[2]] ||
-						   (r[3] < length && 0 < lp_vertex_influence[r[3]]))
-						{
-							if(((uint32 *)lp_channels[i])[j * 4] != integer
-								|| ((uint32 *)lp_channels[i])[j * 4 + 1] != integer
-								|| ((uint32 *)lp_channels[i])[j * 4 + 2] != integer
-								|| (r[3] < length && ((uint32 *)lp_channels[i])[j * 4 + 3] != integer))
-							{
-								if(0 < lp_vertex_influence[r[0]])
-									r[0] = integer;
-								else
-									r[0] = ((uint32 *)lp_channels[i])[j * 4];
-
-								if(0 < lp_vertex_influence[r[1]])
-									r[1] = integer;
-								else
-									r[1] = ((uint32 *)lp_channels[i])[j * 4 + 1];
-
-								if(0 < lp_vertex_influence[r[2]])
-									r[2] = integer;
-								else
-									r[2] = ((uint32 *)lp_channels[i])[j * 4 + 2];
-
-								if(r[3] >= length || 0 < lp_vertex_influence[r[3]])
-									r[3] = integer;
-								else
-									r[3] = ((uint32 *)lp_channels[i])[j * 4 + 3];
-
-								verse_send_g_polygon_set_corner_uint32(node_id, layer_id, j, r[0], r[1], r[2], r[3]);
-							}
-						}
-					}
-				}
-				break;
-				case VN_G_LAYER_POLYGON_CORNER_REAL :
-				{
-					r[0] = lp_vertex_influence[ref[j * 4]];
-					r[1] = lp_vertex_influence[ref[j * 4 + 1]];
-					r[2] = lp_vertex_influence[ref[j * 4 + 2]];
-					r[3] = lp_vertex_influence[ref[j * 4 + 3]];
-					if(r[0] < length && r[1] < length && r[2] < length)
-					{				
-						if(lp_vertex_influence[r[0]] > 0.0001
-							|| lp_vertex_influence[r[1]] > 0.0001
-							|| lp_vertex_influence[r[2]] > 0.0001
-							|| (r[3] < length && lp_vertex_influence[r[3]] > 0.0001))
-						{
-							if(r[3] < length)
-								verse_send_g_polygon_set_corner_real64(node_id, layer_id, j,
-												       ((egreal *)lp_channels[i])[r[0]] * (1.0 - lp_polygon_influence[r[0]]) + value[i] * lp_polygon_influence[r[0]],
-																							((egreal *)lp_channels[i])[r[1]] * (1.0 - lp_polygon_influence[r[1]]) + value[i] * lp_polygon_influence[r[1]],
-																							((egreal *)lp_channels[i])[r[2]] * (1.0 - lp_polygon_influence[r[2]]) + value[i] * lp_polygon_influence[r[2]],
-																							((egreal *)lp_channels[i])[r[3]] * (1.0 - lp_polygon_influence[r[3]]) + value[i] * lp_polygon_influence[r[3]]);
-							else
-								verse_send_g_polygon_set_corner_real64(node_id, layer_id, j,
-												       ((egreal *)lp_channels[i])[r[0]] * (1.0 - lp_polygon_influence[r[0]]) + value[i] * lp_polygon_influence[r[0]],
-												       ((egreal *)lp_channels[i])[r[1]] * (1.0 - lp_polygon_influence[r[1]]) + value[i] * lp_polygon_influence[r[1]],
-												       ((egreal *)lp_channels[i])[r[2]] * (1.0 - lp_polygon_influence[r[2]]) + value[i] * lp_polygon_influence[r[2]], 0);
-
-						}
-					}
-				}
-				break;
-				case VN_G_LAYER_POLYGON_FACE_UINT8 :
-				for(j = 0; j < ref_length; j++)
-					if(lp_polygon_influence[j] > 0.0001)
-						verse_send_g_polygon_set_face_uint8(node_id, layer_id, j, integer);
-				break;
-				case VN_G_LAYER_POLYGON_FACE_UINT32 :
-				for(j = 0; j < ref_length; j++)
-					if(lp_polygon_influence[j] > 0.0001)
-						verse_send_g_polygon_set_face_uint32(node_id, layer_id, j, integer);
-				break;
-				case VN_G_LAYER_POLYGON_FACE_REAL :
-				for(j = 0; j < ref_length; j++)
-					if(lp_polygon_influence[j] > 0.0001)
-						verse_send_g_polygon_set_face_real64(node_id, layer_id, j, value[i]);
-				break;
 			}
+			break;
+			case VN_G_LAYER_VERTEX_UINT32 :
+			for(j = 0; j < length; j++)
+				if(lp_vertex_influence_back[j] < lp_vertex_influence[j] && ((uint32 *)lp_channels[i])[j] != integer)
+					verse_send_g_vertex_set_uint32(node_id, layer_id, j, integer);
+			break;
+			case VN_G_LAYER_VERTEX_REAL :
+			for(j = 0; j < length; j++)
+				if(lp_vertex_influence_back[j] < lp_vertex_influence[j])
+					verse_send_g_vertex_set_real64(node_id, layer_id, j, ((egreal *)lp_channels[i])[j] * (1.0 - lp_vertex_influence[j]) + value[i] * lp_vertex_influence[j]);
+			break;
+			case VN_G_LAYER_POLYGON_CORNER_UINT32 :
+			for(j = 0; j < ref_length; j++)
+			{
+				r[0] = ref[j * 4];
+				r[1] = ref[j * 4 + 1];
+				r[2] = ref[j * 4 + 2];
+				r[3] = ref[j * 4 + 3];
+				if(r[0] < length && r[1] < length && r[2] < length)
+				{				
+					if(0 < lp_vertex_influence[r[0]] ||
+					   0 < lp_vertex_influence[r[1]] ||
+					   0 < lp_vertex_influence[r[2]] ||
+					   (r[3] < length && 0 < lp_vertex_influence[r[3]]))
+					{
+						if(((uint32 *)lp_channels[i])[j * 4] != integer
+							|| ((uint32 *)lp_channels[i])[j * 4 + 1] != integer
+							|| ((uint32 *)lp_channels[i])[j * 4 + 2] != integer
+							|| (r[3] < length && ((uint32 *)lp_channels[i])[j * 4 + 3] != integer))
+						{
+							if(0 < lp_vertex_influence[r[0]])
+								r[0] = integer;
+							else
+								r[0] = ((uint32 *)lp_channels[i])[j * 4];
+
+							if(0 < lp_vertex_influence[r[1]])
+								r[1] = integer;
+							else
+								r[1] = ((uint32 *)lp_channels[i])[j * 4 + 1];
+
+							if(0 < lp_vertex_influence[r[2]])
+								r[2] = integer;
+							else
+								r[2] = ((uint32 *)lp_channels[i])[j * 4 + 2];
+
+							if(r[3] >= length || 0 < lp_vertex_influence[r[3]])
+								r[3] = integer;
+							else
+								r[3] = ((uint32 *)lp_channels[i])[j * 4 + 3];
+							verse_send_g_polygon_set_corner_uint32(node_id, layer_id, j, r[0], r[1], r[2], r[3]);
+						}
+					}
+				}
+			}
+			break;
+			case VN_G_LAYER_POLYGON_CORNER_REAL :
+			{
+				r[0] = lp_vertex_influence[ref[j * 4]];
+				r[1] = lp_vertex_influence[ref[j * 4 + 1]];
+				r[2] = lp_vertex_influence[ref[j * 4 + 2]];
+				r[3] = lp_vertex_influence[ref[j * 4 + 3]];
+				if(r[0] < length && r[1] < length && r[2] < length)
+				{				
+					if(lp_vertex_influence[r[0]] > 0.0001
+						|| lp_vertex_influence[r[1]] > 0.0001
+						|| lp_vertex_influence[r[2]] > 0.0001
+						|| (r[3] < length && lp_vertex_influence[r[3]] > 0.0001))
+					{
+						if(r[3] < length)
+							verse_send_g_polygon_set_corner_real64(node_id, layer_id, j,
+											       ((egreal *)lp_channels[i])[r[0]] * (1.0 - lp_polygon_influence[r[0]]) + value[i] * lp_polygon_influence[r[0]],
+																						((egreal *)lp_channels[i])[r[1]] * (1.0 - lp_polygon_influence[r[1]]) + value[i] * lp_polygon_influence[r[1]],
+																						((egreal *)lp_channels[i])[r[2]] * (1.0 - lp_polygon_influence[r[2]]) + value[i] * lp_polygon_influence[r[2]],
+																						((egreal *)lp_channels[i])[r[3]] * (1.0 - lp_polygon_influence[r[3]]) + value[i] * lp_polygon_influence[r[3]]);
+						else
+							verse_send_g_polygon_set_corner_real64(node_id, layer_id, j,
+											       ((egreal *)lp_channels[i])[r[0]] * (1.0 - lp_polygon_influence[r[0]]) + value[i] * lp_polygon_influence[r[0]],
+											       ((egreal *)lp_channels[i])[r[1]] * (1.0 - lp_polygon_influence[r[1]]) + value[i] * lp_polygon_influence[r[1]],
+											       ((egreal *)lp_channels[i])[r[2]] * (1.0 - lp_polygon_influence[r[2]]) + value[i] * lp_polygon_influence[r[2]], 0);
+					}
+				}
+			}
+			break;
+			case VN_G_LAYER_POLYGON_FACE_UINT8 :
+			for(j = 0; j < ref_length; j++)
+				if(lp_polygon_influence[j] > 0.0001)
+					verse_send_g_polygon_set_face_uint8(node_id, layer_id, j, integer);
+			break;
+			case VN_G_LAYER_POLYGON_FACE_UINT32 :
+			for(j = 0; j < ref_length; j++)
+				if(lp_polygon_influence[j] > 0.0001)
+					verse_send_g_polygon_set_face_uint32(node_id, layer_id, j, integer);
+			break;
+			case VN_G_LAYER_POLYGON_FACE_REAL :
+			for(j = 0; j < ref_length; j++)
+				if(lp_polygon_influence[j] > 0.0001)
+					verse_send_g_polygon_set_face_real64(node_id, layer_id, j, value[i]);
+			break;
 		}
 	}
 	if(lp_vertex_influence != NULL)
-		for(i = 0; i < length; i++)
-			lp_vertex_influence_back[i] = lp_vertex_influence[i];
+		memcpy(lp_vertex_influence_back, lp_vertex_influence, length * sizeof *lp_vertex_influence_back);
 	if(lp_polygon_influence != NULL)
-		for(i = 0; i < ref_length; i++)
-			lp_polygon_influence_back[i] = lp_polygon_influence[i];
+		memcpy(lp_polygon_influence_back, lp_polygon_influence, length * sizeof *lp_polygon_influence_back);
 }
